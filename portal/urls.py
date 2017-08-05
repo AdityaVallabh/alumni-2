16abe33bbd8f3ae6f0428aa096bad875fc6ccdad
Chaitanya <cayadav2009@gmail.com>
2017-08-06 01:17:03 +0530

    Add Work experience route

--- portal/urls.py
+++ portal/urls.py
@@ -7,10 +7,12 @@ urlpatterns = [
     url(r'^(?P<username>\w+)/basic', views.view_basic, name='get-user-basic'),
     url(r'^(?P<username>\w+)/social', views.view_social, name='get-user-social'),
     url(r'^(?P<username>\w+)/misc', views.view_misc, name='get-user-misc'),
+    url(r'^(?P<username>\w+)/work_experience', views.view_work_experience, name='get-work-experience'),
 
     url(r'^basic/edit', views.update_basic, name='edit-user-basic'),
     url(r'^social/edit', views.update_social, name='edit-user-social'),
     url(r'^misc/edit', views.update_misc, name='edit-user-misc'),
+    url(r'^work-experience/edit', views.update_work_experience, name='edit-user-work-experience'),
     # url(r'^(?P<username>\w+)/permanent_address/edit', views.PostInfo.permanent_address, name='edit-permanent-address'),
     # url(r'^(?P<username>\w+)/current_address/edit', views.PostInfo.current_address, name='edit-current-address'),
 ]
